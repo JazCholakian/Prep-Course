95348c92ce9c8da69c1c5251e34e96c1fc977795
Jazmín <104220393+JazCholakian@users.noreply.github.com>
2022-06-11 11:31:43 -0500

    finished homework!

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -246,7 +246,7 @@ function esVocal(letra){
   if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
     return "Es vocal"
   }
-  return "Dato incorrecto"git
+  return "Dato incorrecto"
 }
 
 
